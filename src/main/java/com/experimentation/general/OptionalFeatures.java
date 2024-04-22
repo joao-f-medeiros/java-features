a42d20c43839d4911456780fa8871acab94078ae
João Medeiros <joaomedeiros@MacBook-Air-de-Thaisa.local>
2024-04-22 15:11:20 -0300

    Includes Optional Features

---
+++ src/main/java/com/experimentation/general/OptionalFeatures.java
@@ -0,0 +1,25 @@
+package com.experimentation.general;
+
+import java.util.Optional;
+import java.util.OptionalInt;
+
+public class OptionalFeatures {
+
+  public static void main(String[] args) {
+    String s = Optional.of("29").orElse("0");
+    Integer number = convertToNumber(s).orElse(1);
+
+    System.out.println(number);
+
+    int numberConverted = Integer.parseInt(s);
+    OptionalInt integer = OptionalInt.of(numberConverted);
+    System.out.println(integer.getAsInt());
+
+    Optional.of(number).orElseThrow(() -> new IllegalArgumentException());
+  }
+
+  public static Optional<Integer> convertToNumber(String numberStr) {
+    Integer number = Integer.valueOf(numberStr);
+    return Optional.of(number);
+  }
+}
