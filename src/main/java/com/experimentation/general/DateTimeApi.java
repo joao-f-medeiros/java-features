0eb8e8b2b003c5808b120fd64514e22301b11e26
João Medeiros <joaomedeiros@MacBook-Air-de-Thaisa.local>
2024-04-22 17:11:51 -0300

    Implements Date Time Apis Examples

---
+++ src/main/java/com/experimentation/general/DateTimeApi.java
@@ -0,0 +1,52 @@
+package com.experimentation.general;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+public class DateTimeApi {
+
+  public static void main(String[] args) {
+    // LocalDate - Represents only a date without anything else
+    LocalDate ldNow = LocalDate.now();
+    System.out.println(ldNow);
+    LocalDate birthday = LocalDate.of(1986, Month.OCTOBER, 31);
+    System.out.println(birthday);
+
+    // LocalTime - Represents only a time without anything else
+    LocalTime ltNow = LocalTime.now();
+    System.out.println(ltNow);
+    LocalTime time = LocalTime.of(10, 30, 40);
+    System.out.println(time);
+
+    // LocalDateTime - Represents only a date and time without anything else
+    LocalDateTime ldtNow = LocalDateTime.now();
+    System.out.println(ldtNow);
+    LocalDateTime birthdayMoment = LocalDateTime.of(birthday, time);
+    System.out.println(birthdayMoment);
+
+    // Instant - Represents an instant in time line always in GMT/UTC time
+    // Very good to represent an instant in time line but without Date information
+    Instant iNow = Instant.now();
+    System.out.println(iNow);
+    Instant i = Instant.ofEpochMilli(500000000000000L);
+    System.out.println(i);
+
+    // ZonedDateTime - Represents a complete calendar, with date, time and timezone
+    // Useful when the timezone information is well known
+    ZonedDateTime zdtNow = ZonedDateTime.now();
+    System.out.println(zdtNow);
+    ZoneId zone = ZoneId.of("America/Montevideo");
+    ZonedDateTime zdt = ZonedDateTime.of(birthdayMoment, zone);
+    System.out.println(zdt);
+
+    // Instant and ZonedDateTime
+    ZonedDateTime atZone = iNow.atZone(ZoneId.of("America/Sao_Paulo"));
+    System.out.println(iNow);
+    System.out.println(atZone);
+  }
+}
---
+++ src/main/java/com/experimentation/general/PeriodDateApi.java
@@ -0,0 +1,53 @@
+package com.experimentation.general;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.Period;
+
+public class PeriodDateApi {
+
+  public static void main(String[] args) {
+    periodFeatures();
+
+    durationFeatures();
+  }
+
+  private static void durationFeatures() {
+    // Duration - Represents a duration in time, using hour, minutes, seconds
+    // We cannot pass Duration to LocaDate
+    Duration ofDays = Duration.ofDays(1);
+    System.out.println(ofDays);
+
+    Duration ofMinutes = Duration.ofMinutes(500);
+    System.out.println(ofMinutes);
+
+    LocalTime ltNow = LocalTime.now();
+    LocalTime lt = LocalTime.of(19, 50, 0);
+    Duration between = Duration.between(ltNow, lt);
+    System.out.println(between);
+  }
+
+  private static void periodFeatures() {
+    // Period - Represents a period of dates in at least one day to years
+    // We cannot pass Period to LocaTime
+    Period period = Period.of(1, 5, 2);
+    System.out.println(period);
+
+    // Immutable - Every change made in a period, must be assigned to a variable
+    Period twoYears = Period.ofYears(2);
+    System.out.println(twoYears);
+
+    LocalDate ld = LocalDate.of(2000, 1, 1);
+    LocalDate ld2 = ld.plusWeeks(2).plusDays(2).plusMonths(3);
+    Period between = Period.between(ld, ld2);
+    System.out.println(between);
+
+    Period between2 = ld.until(ld2);
+    System.out.println(between2);
+
+    // LocalDate - Period can be used to change other objects data
+    LocalDate plus = ld.plus(twoYears);
+    System.out.println(plus);
+  }
+}
