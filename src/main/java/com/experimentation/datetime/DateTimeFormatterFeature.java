3cb9fcee5cf2f6287c1600a7193c0e244eb15838
João Medeiros <joaomedeiros@MacBook-Air-de-Thaisa.local>
2024-04-23 11:53:41 -0300

    Inclues problem of old dates and LMT

--- src/main/java/com/experimentation/datetime/DateTimeFormatterFeature.java
+++ src/main/java/com/experimentation/datetime/DateTimeFormatterFeature.java
@@ -1,17 +1,21 @@
 package com.experimentation.datetime;
 
+import java.text.DateFormat;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 import java.time.temporal.TemporalAccessor;
+import java.util.Date;
 import java.util.Locale;
 
 public class DateTimeFormatterFeature {
 
   public static void main(String[] args) {
-    Locale.setDefault(Locale.US);
+    Locale.setDefault(Locale.forLanguageTag("pt-BR"));
 
     DateTimeFormatter formatterFull = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.FULL);
 
@@ -36,5 +40,32 @@ public class DateTimeFormatterFeature {
     System.out.println(ltParsed);
     LocalTime ltFrom = LocalTime.from(ltParsed);
     System.out.println(ltFrom);
+
+    problemOfLMTForOldDates();
+  }
+
+  /*
+   * Before 1900, there weren't a clear definition of the correct time zones time
+   * that we have today. To solve this, the date used considers the LMT - Local
+   * Mean Time that has a try to find the correct differences. The Date API
+   * doesn't use the LMT but the new Date Time API does. That's why the
+   * difference.
+   */
+  private static void problemOfLMTForOldDates() {
+    DateTimeFormatter formatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM);
+    DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.MEDIUM, DateFormat.MEDIUM);
+
+    LocalDate localDate = LocalDate.of(1899, 8, 6);
+    LocalTime localTime = LocalTime.of(23, 59);
+    ZoneId zoneId = ZoneId.of("America/Sao_Paulo");
+
+    ZonedDateTime zdt = ZonedDateTime.of(localDate, localTime, zoneId);
+    Date date = new Date(zdt.toInstant().toEpochMilli());
+
+    System.out.println(formatter.format(zdt));
+    System.out.println(dateFormat.format(date));
+
+    System.out.println(zdt.getOffset());
+    System.out.println(date.getTimezoneOffset());
   }
 }
