c10387c3fc3e36a394f3355d2c8bd8156d61fac8
João Medeiros <joaomedeiros@MacBook-Air-de-Thaisa.local>
2024-04-24 10:23:55 -0300

    Adds Java 12 new features

--- src/main/java/com/experimentation/general/StringFeatures.java
+++ src/main/java/com/experimentation/general/StringFeatures.java
@@ -3,12 +3,16 @@ package com.experimentation.general;
 public class StringFeatures {
 
   public static void main(String[] args) {
-    String s = "   My Java Code    ";
-    System.out.println(s.isBlank());
-    System.out.println(s.lines());
-    System.out.println(s.repeat(3));
-    System.out.println(s.strip()); // Trim better, should use always
-    System.out.println(s.stripLeading()); // Remove empty space ahead of the String
-    System.out.println(s.stripTrailing()); // Remove empty space before of the String
+    String str = "   My Java Code    ";
+    System.out.println(str.isBlank());
+    System.out.println(str.lines());
+    System.out.println(str.repeat(3));
+    System.out.println(str.strip()); // Trim better, should use always
+    System.out.println(str.stripLeading()); // Remove empty space ahead of the String
+    System.out.println(str.stripTrailing()); // Remove empty space before of the String
+
+    String java12 = "Java 12 Features";
+    System.out.println(java12.indent(2));
+    System.out.println(java12.transform(s -> s + " Nice!").transform(String::toUpperCase));
   }
 }
---
+++ src/main/java/com/experimentation/general/SwitchFeatures.java
@@ -0,0 +1,56 @@
+package com.experimentation.general;
+
+public class SwitchFeatures {
+
+  private enum WEEK_DAY {
+    SUNDAY, MONDAY, TUESDAY, WEDNESSDAY, THURSDAY, FRIDAY, SATURDAY
+  };
+
+  public static void main(String[] args) {
+    System.out.println(oldSwitchCode(WEEK_DAY.MONDAY));
+
+    System.out.println(newSwitchCode(WEEK_DAY.SATURDAY));
+  }
+
+  private static String newSwitchCode(WEEK_DAY day) {
+    String result;
+    // Can use body inside of lambda functions
+    switch (day) {
+      case MONDAY, TUESDAY, WEDNESSDAY, THURSDAY, FRIDAY -> result = "Work Day";
+      case SATURDAY, SUNDAY -> result = "Weekend";
+      default -> {
+        System.out.println("Any code");
+        result = "Day not recognized";
+      }
+    }
+    System.err.println(result);
+
+    // Can not use body inside of lambda functions
+    return switch (day) {
+      case MONDAY, TUESDAY, WEDNESSDAY, THURSDAY, FRIDAY -> "Work Day";
+      case SATURDAY, SUNDAY -> "Weekend";
+      default -> "Day not recognized";
+    };
+  }
+
+  private static String oldSwitchCode(WEEK_DAY day) {
+    String result;
+    switch (day) {
+      case MONDAY:
+      case TUESDAY:
+      case WEDNESSDAY:
+      case THURSDAY:
+      case FRIDAY:
+        result = "Work Day";
+        break;
+      case SATURDAY:
+      case SUNDAY:
+        result = "Weekend";
+        break;
+      default:
+        result = "Day not recognized";
+        break;
+    }
+    return result;
+  }
+}
